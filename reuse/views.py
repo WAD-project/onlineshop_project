78b7c3be489a140e995287a2c0d5ce781e7432f9
angelaweihanng <2485186N@student.gla.ac.uk>
2020-03-31 18:36:53 +0800

    chat with some errors that will be solved in due course

--- reuse/views.py
+++ reuse/views.py
@@ -668,16 +668,17 @@ config = {
 firebase = pyrebase.initialize_app(config)
 auth = firebase.auth()
 
-def singIn(request):
-    return render(request, "signIn.html")
-    def postsign(request):
-        email=request.POST.get('email')
-        passw = request.POST.get("pass")
-        try:
-            user = auth.sign_in_with_email_and_password(email,passw)
-        except:
-            message = "invalid cerediantials"
-        return render(request,"signIn.html",{"msg":message})
+def singIn_chat(request):
+    return render(request, "signIn_chat.html")
+
+def postsign(request):
+    email=request.POST.get('email')
+    passw = request.POST.get("pass")
+    try:
+        user = auth.sign_in_with_email_and_password(email,passw)
+    except:
+        message = "invalid cerediantials"
+        return render(request,"signIn_chat.html",{"msg":message})
     print(user)
-    return render(request, "welcome.html",{"e":email})
+    return render(request, "welcome_chat.html",{"e":email})
             
